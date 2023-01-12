ca2ad801e840d2b1fdd0f0d8f96276edf6811f29
W. Gawroński <wgawronski.wg@gmail.com>
2023-01-12 15:00:32 +0100

    Feature: dodanie do tabeli currencies danych z NBP API

--- nbp-service/app/Services/wgaw/NBPTableAService.php
+++ nbp-service/app/Services/wgaw/NBPTableAService.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Services\wgaw;
 
-use App\Services\Contracts\NBPTableAInterface;
+use App\Models\Currency;
+use Illuminate\Support\Str;
 use App\Services\wgaw\class\CurlClass;
+use App\Services\Contracts\NBPTableAInterface;
+use App\Services\wgaw\interfaces\CurlInterface;
 
 class NBPTableAService implements NBPTableAInterface
 {
@@ -26,7 +29,33 @@ class NBPTableAService implements NBPTableAInterface
 
     public function run(): void
     {
+        $currencies = Currency::all();
         $curl = new CurlClass($this->nbpExchangeRatesTableAUrl);
-        dump($curl->getResponseBody());
+        $rates = $curl->getResponseBody()[0]->rates;
+
+        if ($currencies->isEmpty()) {
+            $this->_insertRates($rates);
+        } else {
+            $this->_insertOrUpdateRates($currencies, $rates);
+        }
+    }
+
+    private function _insertRates(array $rates)
+    {
+        $ratesToInsert = [];
+        foreach ($rates as $rate) {
+            $ratesToInsert[] = [
+                'id' => Str::uuid(),
+                'name' => $rate->currency,
+                'currency_code' => $rate->code,
+                'exchange_rate' => $rate->mid,
+            ];
+        }
+        Currency::insert($ratesToInsert);
+    }
+
+    private function _insertOrUpdateRates($currencies, array $rates)
+    {
+
     }
 }
