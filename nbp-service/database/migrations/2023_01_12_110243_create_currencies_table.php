72656db8449cdcc49dc1969fb42d501a92d25f9a
W. Gawroński <wgawronski.wg@gmail.com>
2023-01-12 12:33:27 +0100

    Currency: model i migracja

---
+++ nbp-service/database/migrations/2023_01_12_110243_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('currency_code', 3);
+            $table->float('exchange_rate', places: 6);
+            $table->integer('exchange_rate_int', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
