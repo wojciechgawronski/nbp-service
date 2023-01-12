59c2635bb3e1e2a50c39626bf21bbab7ffd743d5
W. Gawroński <wgawronski.wg@gmail.com>
2023-01-12 12:50:13 +0100

    Fix: Dodanie UUID do modelu Currency.

--- nbp-service/app/Models/Currency.php
+++ nbp-service/app/Models/Currency.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
 {
     use HasFactory;
+    use HasUuids;
 
     protected $fillable = ['name', 'currency_code', 'exchange_rate', 'exchange_rate_int'];
 }
