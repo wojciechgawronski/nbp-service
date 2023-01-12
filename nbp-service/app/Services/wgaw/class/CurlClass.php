a4e9976d3d658d52d3370d8728c94749acf32a6f
W. Gawroński <wgawronski.wg@gmail.com>
2023-01-12 14:02:23 +0100

    Featre: servis: podstawowa metoda curl()

--- nbp-service/app/Services/wgaw/class/CurlClass.php
+++ nbp-service/app/Services/wgaw/class/CurlClass.php
@@ -9,6 +9,16 @@ use stdClass;
 
 class CurlClass implements CurlInterface
 {
+    private int $httpResponseCode;
+    private string $curlResponse;
+
+    public function __construct(
+        private string $url,
+    )
+    {
+       $this->curlResponse = $this->_curl();
+    }
+
     public function getResponseHeaders(): object
     {
         $o = new stdClass();
@@ -25,6 +35,21 @@ class CurlClass implements CurlInterface
     public function test(): bool
     {
         return false;
+    }
+
+    private function _curl(): string
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        $response = curl_exec($ch);
+        $httpResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $this->httpResponseCode = $httpResponseCode;
+
+        return $response;
     }
 }
 
