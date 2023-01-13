1b4713c797e6d4ccf7c7de35e557601ab922f795
W. Gawroński <wgawronski.wg@gmail.com>
2023-01-13 12:35:00 +0100

    Widok: float vs int

--- nbp-service/resources/views/site_elements/table_currencies.blade.php
+++ nbp-service/resources/views/site_elements/table_currencies.blade.php
@@ -1,5 +1,12 @@
 @if($currencies->isNotEmpty())
 
+    <?php
+    $converter = 10_000;
+    $amount = 1000_000_000;
+    ?>
+
+    <p>przelicznik (na int): {{ $converter }}, ({{ Str::length($converter)-1 }})  | ilość: {{ $amount }} ({{ Str::length($amount)-1 }}) </p>
+
 <table class="table table-sm table-striped">
     <thead>
         <tr>
@@ -9,7 +16,7 @@
             <th scope="col">code</th>
             <th scope="col">rate (float)</th>
             <th scope="col">1 mld</th>
-            <th scope="col">rate (int)</th>
+            <th scope="col">rate (int, * 10000)</th>
             <th scope="col">1 mld</th>
             <th scope="col">Roznica: float - int</th>
             <th scope="col"></th>
@@ -17,16 +24,20 @@
     </thead>
     <tbody>
         @foreach($currencies as $item)
+            <?php
+            $floatCost = $item->exchange_rate * $amount;
+            $intCost = ($item->exchange_rate_int/$converter)*$amount;
+            ?>
             <tr>
                 <th scope="row">{{ $loop->iteration }}</th>
                 <td>{{ Str::limit($item->id, 4, '..') }}</td>
                 <td>{{ $item->name }}</td>
                 <td>{{ $item->currency_code }}</td>
                 <td>{{ $item->exchange_rate }}</td>
-                <td>-</td>
+                <td>{{ $floatCost }}</td>
                 <td>{{ $item->exchange_rate_int }}</td>
-                <td>-</td>
-                <td>-</td>
+                <td>{{ $intCost }}</td>
+                <td>{{ $floatCost - $intCost }}</td>
             </tr>
         @endforeach
     </tbody>
