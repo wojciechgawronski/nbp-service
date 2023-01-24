4a0227fbc7f7d1815933d8232501452790941759
W. Gawroński <wgawronski.wg@gmail.com>
2023-01-24 08:53:12 +0100

    feat: dodanie globalnych funkcji

---
+++ nbp-service/app/Services/wgaw/my_functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function _print($var)
+{
+    echo "<pre>";
+    print_r($var);
+    echo "</pre>";
+}
+
+function _dump($var)
+{
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+}
